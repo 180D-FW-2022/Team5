95cf35d906cbc39d0177796190c52df87163bd9a
Isha G <igonugun@gmail.com>
2022-11-26 12:12:55 -0800

    background speech detect and config changes

--- main_control.py
+++ main_control.py
@@ -2,13 +2,16 @@ import json
 from suggest import *
 from dummy import *
 from firebase_rt import *
+from ed_speech import SpeechDetect
 import firebase_admin
 from firebase_admin import credentials, storage, db
+import threading
 
 class Main_Control:
     def __init__(self):
         with open('config.txt') as f:
             data = f.read()
+            f.close()
 
         settings = json.loads(data)
 
@@ -16,31 +19,47 @@ class Main_Control:
         self.user = settings["user_id"]
 
         self.database = Database(self.user)
+        self.speech = SpeechDetect(self.should_suggest)
 
         self.ref = self.database.get_ref()
 
+    def change_config(self):
+        f = open('config.txt', "w")
+        dictionary = {"user_id": self.user, "enable_suggest": self.should_suggest}
+        f.write(json.dumps(dictionary))
+        f.close()
+
     def run(self):
+        suggest = True
+        t1 = threading.Thread(target=self.speech.detect_speech, daemon=True)
+        t1.start()
         while(1):
-            print(self.should_suggest)
+            # check if we should provide audio suggestions
+            should = self.speech.suggestSetting()
+
+            # update config if settings changed
+            if should != self.should_suggest:
+                self.should_suggest = should
+                self.change_config()
 
             # get current speed
             speed = curr_speed()
             acc = curr_acc()
             speedWarn = speed > 65
             self.database.uploadData(speed, acc, speedWarn)
-            if speedWarn:
+            if self.should_suggest and speedWarn:
                 warn_speed(65, round(speed,2))
 
             # check stop sign
-            if stop_sign():
+            if self.should_suggest and stop_sign():
                 approach_stop()
 
             # check gaze
-            if distracted():
+            if self.should_suggest and distracted():
                 driver_distracted()
 
             # check stop blown
-            if stop_blown():
+            if self.should_suggest and stop_blown():
                 blew_stop()
 
 

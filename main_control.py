7f46e2bde4d97a469a6374301801f9fd5b5da84e
Blake Lazarine <blake.l.lazarine@gmail.com>
2022-11-30 05:22:45 +0000

    removed firebase

--- main_control.py
+++ main_control.py
@@ -1,11 +1,11 @@
 import json
 import sys
-from suggest import *
+from suggestdummy import *
 from dummy import *
-from firebase_rt import *
+#from firebase_rt import *
 from ed_speech import SpeechDetect
-import firebase_admin
-from firebase_admin import credentials, storage, db
+#import firebase_admin
+#from firebase_admin import credentials, storage, db
 import threading
 import comms.uart_proc as uart_utils
 import comms.uart_rec as uart_receiver
@@ -21,10 +21,10 @@ class Main_Control:
         self.should_suggest = settings["enable_suggest"]
         self.user = settings["user_id"]
 
-        self.database = Database(self.user)
+        #self.database = Database(self.user)
         self.speech = SpeechDetect(self.should_suggest)
 
-        self.ref = self.database.get_ref()
+        #self.ref = self.database.get_ref()
         self.ser = uart_utils.initialize_serial()
 
         # message sent by Device 1 (TX/RX pins 9/10) and Device 2 (TX/RX pins 7/8)
@@ -74,7 +74,7 @@ class Main_Control:
             speed = curr_speed()
             acc = curr_acc()
             speedWarn = speed > 65
-            self.database.uploadData(speed, acc, speedWarn)
+            #self.database.uploadData(speed, acc, speedWarn)
             if self.should_suggest and speedWarn:
                 warn_speed(65, round(speed,2))
 
@@ -83,8 +83,8 @@ class Main_Control:
                 approach_stop()
 
             # check gaze
-            if self.should_suggest and distracted(self.d2msg):
-                driver_distracted()
+            #if self.should_suggest and distracted(self.d2msg):
+            #    driver_distracted()
 
             # check stop blown
             if self.should_suggest and stop_blown():

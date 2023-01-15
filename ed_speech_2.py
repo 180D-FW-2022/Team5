83c7f4aa97d7da26ae9fd6036817a676d11d959a
Isha G <igonugun@gmail.com>
2023-01-14 21:28:46 -0800

    minor fixes

--- ed_speech_2.py
+++ ed_speech_2.py
@@ -14,6 +14,7 @@ class SpeechDetect():
         print("Starting Speech Detection")
         while True:
             r = sr.Recognizer()
+            text = ""
             with sr.Microphone() as source:
                 audio = r.listen(source)
             try:
@@ -22,6 +23,10 @@ class SpeechDetect():
                 print("Google Speech Recognition could not understand audio")
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+            if not text:
+                print("oops")
+                continue
 
             if 'hey ed' in text:
                 expect_command = True

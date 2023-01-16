fcbdae7588d32fe2484558853f5beefffdfb13e4
Isha G <igonugun@gmail.com>
2023-01-16 01:31:00 -0800

    login with google

--- edd-app/src/utils/firebase.js
+++ edd-app/src/utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +22,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+//Enable Google Login
+export const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, googleProvider).then((res) => {
+    console.log(res.user)
+  }).catch((error) => {
+    console.log(error.message)
+  })
+}
+
 export const db = getDatabase(app);

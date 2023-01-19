dfb2de9f676cd3d4a580ccffe46fdeebff42fcaa
Blake Lazarine <blake.l.lazarine@gmail.com>
2023-01-18 21:19:43 -0800

    wrong datatype

--- stop_signs/sign_tracker.py
+++ stop_signs/sign_tracker.py
@@ -68,10 +68,12 @@ class my_detector():
             pred = non_max_suppression(pred, conf_thres, 0.45, None, False, max_det=1000)
 
 
-        modded_pred = np.zeros((pred.shape[0], 3))
-        modded_pred[:,0] = pred[:,0] # x
-        modded_pred[:,1] = pred[:,1] # y
-        modded_pred[:,2] = pred[:,1] - pred[:,3] # height
+        modded_pred = np.zeros((len(pred), 3))
+        for i, det in enumerate(pred):
+            modded_pred[i,0] = det[0]
+            modded_pred[i,1] = det[1]
+            modded_pred[i,2] = det[1] - det[3]
+        
         print(modded_pred[:,2])
 
 

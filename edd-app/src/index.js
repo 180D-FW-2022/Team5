4fe7e45b51e6fb8f384092c408dfb2fa97535871
Isha G <igonugun@gmail.com>
2023-01-25 14:56:24 -0800

    react router and user data

--- edd-app/src/index.js
+++ edd-app/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Data from './Data';
 import Login from './Login';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/dashboard" element={<Data />}/>
+    
+      </Routes>
+    </BrowserRouter>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>
 );
 

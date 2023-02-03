d5cb7b9c73254a5a8781184c498014b7e29f4cec
Brian Hardell <bharde@me.com>
2023-02-03 14:56:02 -0800

    Rewrite the voice speaker to be queue based and threaded so it doesn't bog down the main controller loop waiting for things to be said. Instead dispatches to another thread that handles speaking phrases in the queue

--- suggest.py
+++ suggest.py
@@ -1,42 +1,45 @@
 import pyttsx3
+import threading
+import queue
+import time
 
-engine = pyttsx3.init()
+class Suggest():
+    def __init__():
+        self.engine = pyttsx3.init()
+        self.q = queue.Queue()
 
-def approach_stop():
-    text = "Approaching stop sign. Start slowing down!"
-    engine.say(text)
-    engine.runAndWait()
+        self.runner = threading.Thread(target=self.__run)
+        self.runner.start()
 
-def warn_speed(limit, speed):
-    text = "Slow down! Speed limit is " + str(limit) + "mph. Your speed is " + str(speed) + "mph."
-    engine.say(text)
-    engine.runAndWait()
+    def __run(self):
+        while True:
+            if not self.q.empty():
+                text = q.get()
+                engine.say(text)
+                engine.runAndWait()
+            time.sleep(0.05)
 
-def blew_stop():
-    text = "Start slowing down sooner next time"
-    engine.say(text)
-    engine.runAndWait()
 
-def driver_distracted():
-    text = "Keep your eyes on the road!"
-    engine.say(text)
-    engine.runAndWait()
+    def approach_stop(self):
+        self.q.put("Approaching stop sign. Start slowing down!")
 
-def disable_suggestions():
-    text = "Suggestions disabled"
-    engine.say(text)
-    engine.runAndWait()
+    def warn_speed(self, limit, speed):
+        self.q.put("Slow down! Speed limit is " + str(limit) + "mph. Your speed is " + str(speed) + "mph.")
 
-def enable_suggestions():
-    text = "Suggestions enabled"
-    engine.say(text)
-    engine.runAndWait()
+    def blew_stop(self):
+        self.q.put("Start slowing down sooner next time")
 
-def power_off():
-    text = "Ending session"
-    engine.say(text)
-    engine.runAndWait()
+    def driver_distracted(self):
+        self.q.put("Keep your eyes on the road!")
 
-def say_phrase(text):
-    engine.say(text)
-    engine.runAndWait()
+    def disable_suggestions(self):
+        self.q.put("Suggestions disabled")
+
+    def enable_suggestions(self):
+        self.q.put("Suggestions enabled")
+
+    def power_off(self):
+        self.q.put("Ending session")
+
+    def say_phrase(self, text):
+        self.q.put(text)

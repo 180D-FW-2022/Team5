f80467d029c8e011e28d5c96e29926e9f483b7a0
Mike <mikequmrsh@gmail.com>
2023-02-18 00:54:02 -0800

    last interaction time updates

--- speech/SpeechArbitrator.py
+++ speech/SpeechArbitrator.py
@@ -24,7 +24,7 @@ class SpeechArbitrator:
             return -1
         # cooldown to exit when expecting_cmd is on for too long
         if (self.expecting_cmd == True and time.time() - self.t_last_interaction > 5):
-            self.animationPlayer.dequeueAnimation()
+            self.animationPlayer.clearAnimation()
             self.expecting_cmd = False
         if (self.speechmap[phrase_id] == 'hey ed '):
             # TODO: add a timeout such that when the user says hey ed, and no valid speech is detected, return to not expect cmd
@@ -39,21 +39,24 @@ class SpeechArbitrator:
         if self.expecting_cmd:
             if self.speechmap[phrase_id] == 'power off ':
                 print("turning off")
+                self.t_last_interaction = time.time()
                 self.expecting_cmd = False
                 return 1
             if self.speechmap[phrase_id] == 'stop ':
                 print("Disabling suggestions")
+                self.t_last_interaction = time.time()
                 self.expecting_cmd = False
                 return 4
             if self.speechmap[phrase_id] == 'enable ':
                 print("Enabling suggestions")
+                self.t_last_interaction = time.time()
                 self.expecting_cmd = False
                 return 3
             if self.speechmap[phrase_id] == 'report ':
                 print("Providing Summary")
+                self.t_last_interaction = time.time()
                 self.expecting_cmd = False
                 return 2
-            self.t_last_interaction = time.time()
 
         return 0
 

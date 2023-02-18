e786cb4c8946e64fdfd154cc3e8e33db9374c4fb
Mike <mikequmrsh@gmail.com>
2023-02-18 00:33:35 -0800

    test case 1 delay change

--- mock/MockSpeechDetector.py
+++ mock/MockSpeechDetector.py
@@ -10,20 +10,20 @@ class MockSpeechDetector:
         print("Running scheme " + str(self.scheme) + " of MOCK SPEECH DETECTOR")
         if (self.scheme == 1):
             if (step == 0):
-                time.sleep(1)
+                time.sleep(3)
                 return "1" # HEY ED! light up
             if (step == 1):
-                time.sleep(1)
+                time.sleep(3)
                 return "4" # ENABLE! light up
             if (step == 2):
                 time.sleep(3)
                 return "3" # STOP! no light, due to no "hey ed" utterance
             if (step == 3):
-                time.sleep(1)
+                time.sleep(3)
                 return "1" # HEY ED! light up
             if (step == 4):
-                time.sleep(1)
-                return "4" # STOP! light up
+                time.sleep(3)
+                return "3" # STOP! light up
             else:
                 return None
             

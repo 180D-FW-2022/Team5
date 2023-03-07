78f0799f98a4986632aedafd47dd246499d0b37c
Brian Hardell <bharde@me.com>
2023-03-06 20:12:48 -0800

    Some changes

--- speech/SpeechDetector.py
+++ speech/SpeechDetector.py
@@ -5,9 +5,8 @@ import time
 class SpeechDetector():
     def __init__(self):
         self.r = sr.Recognizer()
-        self.m = sr.Microphone(2)
 
-        with self.m as source:
+        with sr.Microphone() as source:
             self.r.adjust_for_ambient_noise(source)
             self.r.non_speaking_duration = 0.26
             self.r.pause_threshold = 0.26
@@ -18,45 +17,47 @@ class SpeechDetector():
         print("Starting Speech Detection")
         text = ""
         while True:
-            print("listening...")
-            audio = self.r.listen(self.m, phrase_time_limit=3.0)
-            try:
-                text = self.r.recognize_google(audio).lower()
-            except sr.UnknownValueError:
-                print("Google Speech Recognition could not understand audio")
-                text = ""
-            except sr.RequestError as e:
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
-                text = ""
-            if not text:
-                print("oops")
-                text = ""
-                continue
+            with sr.Microphone() as source:
+                print("listening...")
+                audio = self.r.listen(source, phrase_time_limit=2.5)
+                print("Done. Sending to API...")
+                try:
+                    text = self.r.recognize_google(audio).lower()
+                except sr.UnknownValueError:
+                    print("Google Speech Recognition could not understand audio")
+                    text = ""
+                except sr.RequestError as e:
+                    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                    text = ""
+                if not text:
+                    print("oops")
+                    text = ""
+                    continue
 
-            if text != "":
-                print(text)
-                break
-            # more than 5 seconds listening
-            if time.time() - t0 > 2.5:
-                break
+                if text != "":
+                    print(text)
+                    break
+                # more than 5 seconds listening
+                if time.time() - t0 > 2.5:
+                    break
 
-            '''
-            if 'hey ed' in text:
-                text = ""
-                print("wakeword")
-            if 'power off' in text:
-                text = ""
-                print("turning off")
-            if 'stop' in text:
-                text = ""
-                print("Disabling suggestions")
-            if 'enable' in text:
-                text = ""
-                print("Enabling suggestions")
-            if 'report' in text:
-                text = ""
-                print("Providing Summary")
-            '''
+                '''
+                if 'hey ed' in text:
+                    text = ""
+                    print("wakeword")
+                if 'power off' in text:
+                    text = ""
+                    print("turning off")
+                if 'stop' in text:
+                    text = ""
+                    print("Disabling suggestions")
+                if 'enable' in text:
+                    text = ""
+                    print("Enabling suggestions")
+                if 'report' in text:
+                    text = ""
+                    print("Providing Summary")
+                '''
                 
 if __name__ == '__main__':
     s =  SpeechDetector()
